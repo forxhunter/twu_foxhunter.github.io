68e74894e823af7d9871abcec1eb633781ff10a0
forxhunter <41635675+forxhunter@users.noreply.github.com>
2025-08-01 14:50:42 +0000

    Deploying to gh-pages from @ forxhunter/forxhunter.github.io@a8b02848450827262929821bd997fa494a644c17 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,30 +30,23 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-",
+          title: '',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
+          section: "News",},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
+          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-talk-in-ipols-2025-about-spatial-heterogeneity-alters-the-dynamics-of-the-yeast-galactose-switch",
+          title: 'Talk in iPoLS 2025 about Spatial Heterogeneity Alters the Dynamics of the Yeast...',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-project-1",
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
